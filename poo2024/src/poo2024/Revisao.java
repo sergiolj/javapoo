c6795cc70dcd44971d952fda8cd20c7cc7315f41
Sergio Lopes Junior <sergiolj@gmail.com>
2024-08-13 09:08:10 -0300

    Primeira aula

---
+++ poo2024/src/poo2024/Revisao.java
@@ -0,0 +1,11 @@
+package poo2024;
+
+public class Revisao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0;i<=10;i+=2) {
+			System.out.print(i!=10? i+",":i);
+		}
+	}
+}
