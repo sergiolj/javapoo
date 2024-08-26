70cba0ad5636238c38d816f04f63ddacb858bb10
Sergio Lopes Junior <sergiolj@gmail.com>
2024-08-26 14:03:06 -0300

    Primeiros exercicios classes e objetos

--- poo2024/src/poo2024/Revisao.java
+++ poo2024/src/poo2024/Revisao.java
@@ -3,9 +3,12 @@ package poo2024;
 public class Revisao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		for (int i=0;i<=10;i+=2) {
 			System.out.print(i!=10? i+",":i);
+		}
+		System.out.println();
+		for (int i=9;i>=-9;i-=3) {
+			System.out.print(i!=-9? i+",":i);
 		}
 	}
 }
