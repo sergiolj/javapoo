d0678b784b679406d1b83bdd11ac3cbd01033850
Sergio Lopes Junior <sergiolj@gmail.com>
2024-08-28 14:14:19 -0300

    Outer e Inner Classes

--- poo2024/src/poo2024/Computador.java
+++ poo2024/src/poo2024/Computador.java
@@ -1,24 +1,39 @@
 package poo2024;
 
-public class Computador {
+public class Computador { //OUTER CLASSE COMPUTADOR
 	double preco;
-		class Processador{
+		class Processador{ // INNER CLASSE PROCESSADOR
 		int cores;
 		String fabricante;
-			int getcacheL1() {
-				return 1024;
-			}
+		int cacheL1;
+		public Processador() {} // CONSTRUTOR PADRAO
+		public Processador(String fabricante, int cores, int cacheL1) { //CONSTRUTOR CUSTOMIZADO DA INNER CLASSE PROCESSADOR
+			this.fabricante=fabricante;
+			this.cores=cores;
+			this.cacheL1=cacheL1;
 		}
-		protected class RAM{
+		public void descricao() { //MÉTODO DE DESCRIÇÃO DA INNER CLASSE PROCESSADOR DE COMPUTADOR
+			System.out.print(this.fabricante+"/");
+			System.out.print(this.cores+"/");
+			System.out.print(this.cacheL1+"/");
+		}
+		}
+		protected class RAM{ //INNER CLASSE PROTEGIDA RAM
 			int capacidade;
 			String fabricante;
 			int unidades;
-				double getVelocidadeClock() {
-					return 5.3;
-				}
-				int getCapacidade() {
-					return this.capacidade;
-				}
+			double getVelocidadeClock() {
+				return 5.3;
+			}
+			int getCapacidade() {
+				return this.capacidade;
+			}
+			public void descricao() { // MÉTODO DE DESCRIÇÃO DA INNER CLASSE RAM DE COMPUTADOR
+				System.out.println(this.fabricante);
+				System.out.println(this.capacidade);
+				System.out.println(this.unidades);
+				System.out.println(this.getVelocidadeClock());
+			}
 		}
 		public void descricao() {
 
@@ -34,12 +49,9 @@ public class Computador {
 		 */
 		
 		public static void main(String[] args) {
-			//CRIA UM OBJETO DA CLASSE EXTERNA COMPUTADOR
-			//CRIA O MEU PCGAMER
-				Computador pcGamer = new Computador();
-			//CRIA UM OBJETO DA CLASSE INTERNA 
-			//CRIA O PROCESSADOR DO MEU PCGAMER
-				Computador.Processador meuRyzen9 = pcGamer.new Processador();
+				Computador pcGamer = new Computador();//CRIA O MEU PCGAMER DA CLASSE COMPUTADOR
+				Computador.Processador meuRyzen5 = pcGamer.new Processador();//CRIA O PROCESSADOR ANTIGO DO MEU PCGAMER COM CONSTRUTOR PADRAO
+				Computador.Processador meuRyzen9 = pcGamer.new Processador("AMD",16,2048);
 				Computador.RAM minhaRAM = pcGamer.new RAM();
 				pcGamer.preco=10000;
 				meuRyzen9.cores=12;
@@ -47,5 +59,12 @@ public class Computador {
 				System.out.println("Custo do computador R$: "+pcGamer.preco);
 				System.out.println("Processador de "+meuRyzen9.cores+" núcleos");
 				System.out.println("Memória RAM: "+minhaRAM.getCapacidade()+"Gb");
+				System.out.println("  ");
+				minhaRAM.descricao();
+				System.out.print("Meu Ryzen 5: ");
+				meuRyzen5.descricao();
+				System.out.println("");
+				System.out.print("Meu Ryzen 9: ");
+				meuRyzen9.descricao();
 		}
 }
